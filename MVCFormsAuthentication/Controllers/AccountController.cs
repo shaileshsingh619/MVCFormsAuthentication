17b1f0c808a5ef2cc7e1b3b340f321debb23cb76
shaileshsingh619 <shaileshsingh3979@gmail.com>
2019-04-02 18:57:47 +0530

    Add project files.

---
+++ MVCFormsAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using MVCFormsAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCFormsAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                bool isValid = context.Users.Any(x => x.Username == model.Username.Trim() && x.Password == model.Password.Trim());
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username.Trim(), false);
+                    return RedirectToAction("index","employees");
+                }else
+                {
+                    ModelState.AddModelError("","Invalid Username or password.");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(Users model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("login");
+        }
+
+       
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
